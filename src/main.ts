a96c1fff6d6d18bded8ce1fa5a110b6b05a6c4af
zhangmin <zhangm@suntang.com>
2023-07-03 15:26:36 +0800

    解决App already provides property with key "Symbol(pinia)"警告问题

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+// import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
@@ -9,7 +9,7 @@ import store from './stores/index'
 
 const app = createApp(App)
 
-app.use(createPinia())
+// app.use(createPinia())
 app.use(router)
 app.use(store)
 app.mount('#app')
